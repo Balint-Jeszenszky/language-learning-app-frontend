4fa5e00819dd57adfbc2fcba7a95ff0d4d4bcd75
Bálint Jeszenszky <kisjeszeni1234@gmail.com>
2022-11-10 22:32:52 +0100

    scores displayed on chart

--- src/app/components/course/course-details/course-details.component.ts
+++ src/app/components/course/course-details/course-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import Chart from 'chart.js/auto';
 import { CourseService } from 'src/app/services/course.service';
 import { CourseDetails, WordPair } from 'src/app/services/types';
 import { WordPairService } from 'src/app/services/word-pair.service';
@@ -13,6 +14,7 @@ export class CourseDetailsComponent implements OnInit {
   wordPairs?: WordPair[];
   courseDetails?: CourseDetails;
   displayedColumns: string[] = ['name', 'email', 'score'];
+  chart?: Chart;
 
   constructor(
     private readonly route: ActivatedRoute,
@@ -29,7 +31,43 @@ export class CourseDetailsComponent implements OnInit {
           courseDetails.deadline = new Date(courseDetails.deadline);
         }
         this.courseDetails = courseDetails;
+        this.createChart();
       });
+    });
+  }
+
+  createChart() {
+    if (!this.wordPairs) {
+      return;
+    }
+
+    const scores = new Array(this.wordPairs.length + 1).fill(0);
+    this.courseDetails?.students.forEach(s => {
+      scores[s.score || 0]++;
+    });
+
+    this.chart = new Chart("score", {
+      type: 'bar',
+      data: {
+        labels: Object.keys(scores), 
+	       datasets: [
+          {
+            label: "Scores",
+            data: scores,
+            backgroundColor: '#3f51b5',
+          }
+        ]
+      },
+      options: {
+        aspectRatio: 2.5,
+        scales: {
+          y: {
+            ticks: {
+              precision: 0,
+            }
+          }
+        }
+      }
     });
   }
 
